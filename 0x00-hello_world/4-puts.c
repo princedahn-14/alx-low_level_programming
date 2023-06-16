18ba8f4147b3d73e6915ad23ad8f60258aaad8cd
princedahn-14 <jaydahn278@gmail.com>
2023-06-15 19:34:22 -0700

    make new changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with mutiple port function
+ * Return: 0 (Great)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
